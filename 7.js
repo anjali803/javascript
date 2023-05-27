393df320422e3125df612cc60d84a66824fcd94d
Anjali N V <anjaliprathees.nv@gmail.com>
2023-05-27 07:29:51 +0530

    first commit

---
+++ 7.js
@@ -0,0 +1,20 @@
+console.log("Javascript functions");
+
+//javascript function is a block of code designed to perform a particular task
+//javascript function execute when something invokes it
+
+//function for computing the product of two numbers
+function mul(p1,p2){
+    return p1 * p2
+
+}
+let product = mul(2,4);
+console.log(product + " is the product we obtained on multiplying ")
+
+//sum of two numbers
+function sum(a,b){
+    let c = a+b
+    return c;
+}
+let result = sum(10,10)
+console.log(result)
